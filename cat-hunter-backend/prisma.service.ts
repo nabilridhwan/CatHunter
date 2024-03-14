d65768bb7241104e0d13435f7ceb2a12ff34fff7
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-14 20:26:34 +0800

    fix: remove ts-ignore in prisma.service.ts

--- cat-hunter-backend/prisma.service.ts
+++ cat-hunter-backend/prisma.service.ts
@@ -1,7 +1,6 @@
 import {Injectable, OnModuleInit} from '@nestjs/common';
 import {PrismaClient} from '@prisma/client';
 
-// @ts-ignore
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
