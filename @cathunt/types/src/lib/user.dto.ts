1e3a93a208e3dc55890d7397030e2c49f70e1363
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-14 20:18:34 +0800

    feat: update create user dto to include name

--- @cathunt/types/src/lib/user.dto.ts
+++ @cathunt/types/src/lib/user.dto.ts
@@ -2,9 +2,10 @@ import {z} from 'zod'
 import {createZodDto} from "nestjs-zod";
 
 export const CreateUserSchema = z.object({
+  name: z.string().min(3).max(255),
   username: z.string(),
-  bio: z.string(),
-  country: z.string(),
+  bio: z.string().optional().default(null),
+  country: z.string().optional().default(null),
 })
 
 export class CreateUserDto extends createZodDto(CreateUserSchema) {
