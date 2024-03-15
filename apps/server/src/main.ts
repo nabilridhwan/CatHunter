a96ed810ade8e349a06f8254bfef8e0d69d6a76b
Nabil Ridhwan <nabridhwan@gmail.com>
2024-03-15 13:56:28 +0800

    fix: enable CORS for server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,15 +3,21 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Enable cors
+  app.enableCors()
+
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
